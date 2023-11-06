e53187a178f3a12aa21d396a75e2d8949ef03206
R3call <thenoobyzombie@gmail.com>
2023-11-06 01:29:00 +0100

    Added custom asset images and folder

--- game/dice.py
+++
@@ -1,5 +0,0 @@
-import random
-
-class Dice:
-    def roll(self):
-        return (random.randint(1, 6), random.randint(1, 6))
--- game/piece.py
+++
@@ -1,4 +0,0 @@
-class Piece:
-    def __init__(self, color):
-        self.color = color
-        # You can add more attributes if needed
