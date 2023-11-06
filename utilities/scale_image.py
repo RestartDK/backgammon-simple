173941764e4815b722a45f408756314732406f4a
R3call <thenoobyzombie@gmail.com>
2023-11-06 11:33:28 +0100

    Added surface type

--- utilities/scale_image.py
+++ utilities/scale_image.py
@@ -1,5 +1,5 @@
 import pygame
 
-def scale_image(image, scale=0.7):
+def scale_image(image: pygame.SurfaceType, scale=0.7):
     return pygame.transform.smoothscale(
         image, (int(image.get_width() * scale), int(image.get_height() * scale)))
