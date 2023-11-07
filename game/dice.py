77fba827a4093c3122a93bcd98c1ab847baa300c
talineshawwaa <talineshawwa15@gmail.com>
2023-11-07 12:25:35 +0100

    Added a comment for render method

--- game/dice.py
+++ game/dice.py
@@ -8,7 +8,17 @@ class Dice:
     def roll(self):
         # Return a tuple of two random integers between 1 and 6
         return (random.randint(1, 6), random.randint(1, 6))
-
+    
+    # This is a render method
     def render(self, screen, position, face_values):
         screen.blit(self.faces[face_values[0] - 1], position)
         screen.blit(self.faces[face_values[1] - 1], (position[0] + self.faces[0].get_width(), position[1]))
+    
+    def reset(self):
+        #resets the counter for the eyes on the dice so that both become zero
+        self.EyeCounter = [0] * 2
+    
+    def theEyeCounter(self, eye):
+        self.EyeCounter = eye 
+
+
