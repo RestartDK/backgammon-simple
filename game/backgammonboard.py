8b37d55292f43ca7125b2172e0c8b48dbefb6aa6
R3call <thenoobyzombie@gmail.com>
2023-11-08 14:43:24 +0100

    Removed constant dependency for screen and use get methods to get height and width, created working game loop, created render method in main game loop

--- game/backgammonboard.py
+++ game/backgammonboard.py
@@ -2,29 +2,30 @@ import pygame
 
 # from utilities.scale_image import scale_image
 
-# Screen constants adjusted to fit the screen
-SCREEN_WIDTH = 1525 #1700
-SCREEN_HEIGHT = 900  #900
-V_LINE_WIDTH = 10  # The width of the v-line
 NUM_TRIANGLES_PER_SIDE = 12
 MIDDLE_AREA_WIDTH = 100  # The width of the area in the middle behind the v-line
 
+
 class BackgammonBoard:
-    def __init__(self):
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
+        self.num_triangles_per_side = 12
+        self.middle_area_width = 100
         self.generate_board()
 
     def generate_board(self):
         # Load and scale background
         self.background = pygame.image.load("assets/images/background.jpg")
         self.background = pygame.transform.smoothscale(
-            self.background, (SCREEN_WIDTH, SCREEN_HEIGHT)
+            self.background, (self.screen.get_width(), self.screen.get_height())
         )
 
         # Load and scale v-line
         self.v_line = pygame.image.load("assets/images/v-line.png")
-        self.v_line = pygame.transform.smoothscale(self.v_line, (5, SCREEN_HEIGHT))
+        self.v_line = pygame.transform.smoothscale(
+            self.v_line, (5, self.screen.get_height())
+        )
 
-        
         # Initialize triangles list
         self.triangles = list()
         for row_idx in range(2):
@@ -35,7 +36,10 @@ class BackgammonBoard:
             ]
             # Scale triangles to match the screen height while maintaining aspect ratio
             scaled_triangles_row = [
-                pygame.transform.smoothscale(tri, (SCREEN_WIDTH // 13, SCREEN_HEIGHT // 2))
+                pygame.transform.smoothscale(
+                    tri,
+                    (self.screen.get_width() // 13, self.screen.get_height() // 2),
+                )
                 for tri in triangles_row
             ]
             self.triangles.append(scaled_triangles_row)
@@ -46,26 +50,28 @@ class BackgammonBoard:
         self.distance_y = 10  # No distance between upper and lower triangles
         # Calculate offsets
         # self.offset_x = (SCREEN_WIDTH - 12 * self.triangle_width - self.v_line.get_width()) // 2 - 50
-        self.offset_x = (SCREEN_WIDTH - 12 * self.triangle_width - MIDDLE_AREA_WIDTH - self.v_line.get_width()) // 2
+        self.offset_x = (
+            self.screen.get_width()
+            - 12 * self.triangle_width
+            - self.middle_area_width
+            - self.v_line.get_width()
+        ) // 2
 
-    def render_board(self, screen):
+    def render(self):
         # Blit the background
-        screen.blit(self.background, (0, 0))
+        self.screen.blit(self.background, (0, 0))
 
         # Blit the triangles
         for idx in range(12):
             # x = idx*self.triangle_width + self.offset_x * (idx // 6)
             x = self.offset_x + idx * self.triangle_width
             if idx >= 6:  # Skip the space for the v-line in the middle
-                x += self.v_line.get_width() + MIDDLE_AREA_WIDTH
+                x += self.v_line.get_width() + self.middle_area_width
             # Blit upper triangles
-            screen.blit(self.triangles[0][idx % 2], (x, 0))
+            self.screen.blit(self.triangles[0][idx % 2], (x, 0))
             # Blit lower triangles
-            screen.blit(self.triangles[1][idx % 2], (x, SCREEN_HEIGHT // 2))
+            self.screen.blit(self.triangles[1][idx % 2], (x, self.screen.get_height() // 2))
 
         # Blit the v-line
-        v_line_x = self.offset_x + 6 * self.triangle_width + MIDDLE_AREA_WIDTH/2
-        screen.blit(self.v_line, (v_line_x, 0))
-
-    def render(self, screen):
-        self.render_board(screen)
+        v_line_x = self.offset_x + 6 * self.triangle_width + self.middle_area_width / 2
+        self.screen.blit(self.v_line, (v_line_x, 0))
--- game/game.py
+++ game/game.py
@@ -1,39 +1,91 @@
 from game.backgammonboard import BackgammonBoard
 from game.dice import Dice
 from game.piece import Piece
+import pygame
 
-# Here initalise the pieces onto the fields on the board in init
+# Screen Constants
+SCREEN_WIDTH = 1525  # 1700
+SCREEN_HEIGHT = 900  # 900
+
 
 class Game:
     def __init__(self):
-        self.board = BackgammonBoard()
+        # Initialise screen for board
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.running = True
+        self.board = BackgammonBoard(self.screen)
         self.dice = Dice()
-        # Add more initializations (players, turn count, etc.)
-    
+        self.initalise_pieces()
+
     def initalise_pieces(self):
         # Define the starting position of all the pieces on the board
-        # Backgammon has specific starting positions for the pieces.
-        positions = {
+        self.piece_positions = {
             # Points are listed for the white player. For black, the points are mirrored.
-            1: [Piece('white') for _ in range(2)],  # white's 24-point
-            6: [Piece('black') for _ in range(5)],  # black's 19-point
-            8: [Piece('black') for _ in range(3)],  # black's 17-point
-            12: [Piece('white') for _ in range(5)],  # white's 13-point
-            13: [Piece('black') for _ in range(5)],  # black's 12-point
-            17: [Piece('white') for _ in range(3)],  # white's 8-point
-            19: [Piece('white') for _ in range(5)],  # white's 6-point
-            24: [Piece('black') for _ in range(2)],  # black's 1-point
+            1: [Piece("white") for _ in range(2)],  # white's 24-point
+            6: [Piece("black") for _ in range(5)],  # black's 19-point
+            8: [Piece("black") for _ in range(3)],  # black's 17-point
+            12: [Piece("white") for _ in range(5)],  # white's 13-point
+            13: [Piece("black") for _ in range(5)],  # black's 12-point
+            17: [Piece("white") for _ in range(3)],  # white's 8-point
+            19: [Piece("white") for _ in range(5)],  # white's 6-point
+            24: [Piece("black") for _ in range(2)],  # black's 1-point
         }
-        
-        # Ensure all points have a list to avoid key errors
-        for point in range(1, 25):
-            if point not in positions:
-                positions[point] = []
-    
-        return positions
-    
+
+        # Calculate positions for each piece
+        self.positions = {}
+        for point, pieces in self.piece_positions.items():
+            self.positions[point] = []
+            for i, piece in enumerate(pieces):
+                # Calculate x position based on the point
+                x_position = (
+                    self.board.offset_x + ((point - 1) % 12) * self.board.triangle_width
+                )
+                if point > 12:  # Adjust for points on the left side of the board
+                    x_position += (
+                        self.board.middle_area_width + self.board.v_line.get_width()
+                    )
+
+                # Calculate y position based on the index of the piece in the stack
+                y_position = (self.board.triangle_height // len(pieces)) * i
+                if point <= 12:  # Pieces on the bottom side
+                    y_position += self.screen.get_height() - self.board.triangle_height
+                # Adjust y_position to start from the top of the triangle
+                y_position += (
+                    (self.screen.get_height() // 2 - self.board.triangle_height)
+                    if point <= 12
+                    else 0
+                )
+
+                # Update piece's position
+                piece.move((x_position, y_position), self.screen)
+                self.add_piece(point, piece)
+
+        return self.positions
+
     def add_piece(self, point, piece: Piece):
-        self.points[point].append(piece)
+        self.positions[point].append(piece)
 
     def remove_piece(self, point):
         return self.points[point].pop() if self.points[point] else None
+
+    def start(self):
+        pygame.init()  # This initializes all the pygame modules
+        # Main loop
+        while self.running:
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # Render the board and pieces
+            self.board.render()  # Call the render method of BackgammonBoard
+
+            # You should also render the pieces here
+            for point, pieces in self.positions.items():
+                for piece in pieces:
+                    piece.update(self.screen)  # Call the update method of each Piece
+
+            pygame.display.flip()
+
+        # Quit Pygame when the main loop ends
+        pygame.quit()
