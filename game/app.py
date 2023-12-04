2bd8be5343f3d6a8a8a036657dd5a8e63fc63c95
R3call <thenoobyzombie@gmail.com>
2023-12-04 23:15:35 +0100

    errand: removed print statement

--- game/app.py
+++ game/app.py
@@ -335,7 +335,6 @@ class App:
                 return True
             
             if not self.eligable_to_move_from_middle(piece, new_point_index):
-                print("Turn has been changed.")
                 self.change_turn()
 
         #Move the piece back to its original position if no other valid move 
